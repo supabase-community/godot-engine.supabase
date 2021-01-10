77b00a285c8dd006f49430cc5f4e3b9a58b08d75
Nicolò Santilio <n.santilio97@gmail.com>
2021-01-10 03:08:00 +0100

    add DatabaseError & AuthenticationError

---
+++ addons/supabase/Auth/auth_error.gd
@@ -0,0 +1,17 @@
+extends Reference
+class_name SupabaseAuthError
+
+var _error : Dictionary
+var type : String = "(empty)"
+var description : String = "(empty)"
+
+func _init(dictionary : Dictionary = {}) -> void:
+	_error = dictionary
+	if not _error.empty():
+		if _error.has("error"):
+			type = _error.error 
+			description = _error.error_description
+		if _error.has("code"):
+			type = str(_error.code)
+			description = _error.msg
+	# different body for same api source ???
