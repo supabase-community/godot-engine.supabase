c3f359adcb18d6e066917c92744f768a6f557fa4
Phil Hudson <>
2023-01-27 15:59:45 +0800

    default error message to empty if null to avoid fatal

--- addons/supabase/Database/database_error.gd
+++ addons/supabase/Database/database_error.gd
@@ -6,7 +6,7 @@ func _init(dictionary : Dictionary = {}) -> void:
 	_error = dictionary
 	if not _error.is_empty():
 		code = _error.code if _error.has("code") else "empty"
-		message = _error.message
+		message = _error.message if _error.has("message") else "empty"
 		hint = _error.hint if _error.has("hint") and _error.hint != null else "empty"
 		details = _error.get("details", "")
 ### always different behavior ???
